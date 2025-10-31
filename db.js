8d3f55631e8cfc946cd1c19e88f9c73568db0bfc
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-10-31 18:34:29 +0100

    chore: change database path to 'db/db.json'

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const dbPath = path.join(__dirname, "db.json");
+const dbPath = path.join(__dirname, "db/db.json");
 
 function readDb() {
   try {
